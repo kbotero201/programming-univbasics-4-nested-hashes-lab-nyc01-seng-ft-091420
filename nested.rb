6f18a267077ef4b7ae2e2a803704a7339d25cfb3
Kevin Botero <kbotero201@gmail.com>
2020-09-09 22:07:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,10 +109,7 @@ def changing_alan
       }
     }
 
-    alans_new_info = "GUI"
-        programmer_hash[:alan_kay][:known_for] = alans_new_info
-        programmer_hash
-end
+  end
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
@@ -134,9 +131,6 @@ def adding_to_dennis
       }
     }
 
-    programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
+
 
 end
