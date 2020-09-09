1b6f41301447923c6822fa9879b9a7c3f600aca0
Kevin Botero <kbotero201@gmail.com>
2020-09-09 22:12:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,13 +112,9 @@ def changing_alan
       }
     }
 
-programmer_hash =
-{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          x:languages => ["C"]
-        }
-     }
+alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for]= alans_new_info
+programmer_hash
 
   end
 
